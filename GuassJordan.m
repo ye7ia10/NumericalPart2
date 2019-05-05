cf6c730614573d1363a2b2370919f2737e5a8bbd
ye7ia10 <yehiaelsayed234@gmail.com>
2019-05-06 00:38:46 +0200

    Gui1 + GaussJordan

---
+++ GuassJordan.m
@@ -0,0 +1,108 @@
+classdef GuassJordan < handle
+    %GUASSJORDAN Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        eqns; %array of equations
+        a;   %matrix of coff.
+        b;   %matrix of constants.
+        vars;
+    end
+    
+    methods
+         function  obj = GuassJordan(obj)
+         end
+         
+       
+       function obj = solve(obj) 
+       [obj.a,obj.b]=equationsToMatrix(sym(obj.eqns));       
+        % m is the row 
+        % c is the column 
+        [m,c]=size(obj.a); 
+        j=1; %j th column 
+        k=1; %k th row 
+        n=1; % n th pivoting row 
+        z=2;% Row operation always starts from 2 
+        % Forward Elimination 
+        % Shifting Loop for column operation 
+        for i=1:c-1 
+        %Making each desired column component zero 
+        for r=z:m 
+        % Checking if any Lower triangle is Already Zero or not 
+        if obj.a(r,j)==0 
+        % If any is zero left the entire row as it is and skip the step 
+        obj.a(r,:)=obj.a(r,:);obj.b(r,:)=obj.b(r,:); 
+        else 
+        obj.b(r,:)=((obj.a(r,j)/obj.a(k,j))*obj.b(n,:))-obj.b(r,:) 
+        obj.a(r,:)=((obj.a(r,j)/obj.a(k,j))*obj.a(n,:))-obj.a(r,:) 
+        end 
+        end 
+        k=k+1;% Changing row after completion of inner loop 
+        n=n+1;% Changing the pivoting row 
+        z=z+1;% Setting a new condition for inner loop 
+        j=j+1;% Changing column after completion of inner loop 
+        end 
+        % Converting all the diagonal elements to one 
+        for row=1:m 
+        obj.b(row,:)=obj.b(row,:)/obj.a(row,row) 
+        obj.a(row,:)=obj.a(row,:)/obj.a(row,row) 
+        end 
+        D=c; 
+        for q=1:D-1 
+        for rowJ=m-1:-1:1 
+        if obj.a(rowJ,c)==0 
+        obj.a(rowJ,:)=obj.a(rowJ,:);obj.b(rowJ,:)=obj.b(rowJ,:); 
+        else 
+        obj.b(rowJ,:)=obj.b(rowJ,:)-(obj.a(rowJ,c)*obj.b(m,:)) 
+        obj.a(rowJ,:)=obj.a(rowJ,:)-(obj.a(rowJ,c)*obj.a(m,:)) 
+        end 
+        end 
+        m=m-1; 
+        c=c-1; 
+        end 
+       end
+       
+       function arr = getVar(obj,str)
+            i = 1;
+            n = length(str);
+            obj.vars = {};
+            while (i <= n)
+                s =  '';
+               if isletter(str(i))
+                   while (i <= n)
+                       i
+                      if isletter(str(i))
+                          s = strcat(s,str(i));
+                          s
+                          i = i+1;
+                          continue;
+                      end
+                      if  ~isnan(str2double(str(i)))
+                          s = strcat(s,str(i));
+                          s
+                          i = i+1;
+                          continue;
+                      end
+                      if str(i) == ' '
+                         s = strcat(s,str(i));
+                         i = i+1;
+                         continue;
+                      else
+                         break;
+                      end
+                      
+                   end
+                   obj.vars{end+1} = s;
+               end
+               i = i+1;
+            end
+            obj.vars 
+       end
+        
+       
+    end
+    
+
+    
+end
+
---
+++ GuiSle.m
@@ -0,0 +1,282 @@
+function varargout = GuiSle(varargin)
+
+
+% Last Modified by GUIDE v2.5 06-May-2019 00:08:01
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @GuiSle_OpeningFcn, ...
+                   'gui_OutputFcn',  @GuiSle_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+%make global array for equations.
+function setGlobalArr(arr)
+global eq
+eq = arr;
+
+function arr = getGlobalArr
+global eq
+arr = eq;
+
+%make global array for variables
+function setGlobalArrVars(arrVar)
+global vars
+vars = arrVar;
+
+function arrVars = getGlobalArrVars
+global vars
+arrVars = vars;
+
+% --- Executes just before GuiSle is made visible.
+function GuiSle_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to GuiSle (see VARARGIN)
+
+% Choose default command line output for GuiSle
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+
+set(handles.initg, 'enable', 'off');
+set(handles.setInit, 'enable', 'off');
+set(handles.nomIt, 'enable', 'off');
+set(handles.eps, 'enable', 'off');
+setGlobalArr({});
+setGlobalArrVars({});
+
+% UIWAIT makes GuiSle wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = GuiSle_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on selection change in MethodChooser.
+function MethodChooser_Callback(hObject, eventdata, handles)
+ contents = cellstr(get(handles.MethodChooser,'String'));
+ choosen = contents{get(handles.MethodChooser,'Value')};
+ if strcmp(choosen, 'Gauss-Seidel')
+        set(handles.initg, 'enable', 'on');
+        set(handles.setInit, 'enable', 'on');
+        set(handles.nomIt, 'enable', 'on');
+        set(handles.eps, 'enable', 'on');
+ else
+        set(handles.initg, 'enable', 'off');
+        set(handles.setInit, 'enable', 'off');
+        set(handles.nomIt, 'enable', 'off');
+        set(handles.eps, 'enable', 'off');
+ end
+ 
+
+% --- Executes during object creation, after setting all properties.
+function MethodChooser_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to MethodChooser (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function eqn_Callback(hObject, eventdata, handles)
+% hObject    handle to eqn (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of eqn as text
+%        str2double(get(hObject,'String')) returns contents of eqn as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function eqn_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to eqn (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in AddEqn.
+function AddEqn_Callback(hObject, eventdata, handles)
+% hObject    handle to AddEqn (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+ choosen = get(handles.eqn, 'String')
+ if isempty(choosen)
+     msgbox('Equation is Empty', 'Error','error');
+     return;
+ end
+ arr = getGlobalArr;
+ arr{end+1} = choosen;
+ setGlobalArr(arr);
+ getVar(choosen);
+ getGlobalArr
+ getGlobalArrVars
+ 
+ 
+
+
+
+function initg_Callback(hObject, eventdata, handles)
+% hObject    handle to initg (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of initg as text
+%        str2double(get(hObject,'String')) returns contents of initg as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function initg_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to initg (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in setInit.
+function setInit_Callback(hObject, eventdata, handles)
+% hObject    handle to setInit (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+
+function nomIt_Callback(hObject, eventdata, handles)
+% hObject    handle to nomIt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of nomIt as text
+%        str2double(get(hObject,'String')) returns contents of nomIt as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function nomIt_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to nomIt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+%get the variables from the equations 
+%add them to array
+%sort them alphapetic
+function arr = getVar(str)
+            i = 1;
+            n = length(str);
+            arrVars = getGlobalArrVars;
+            while (i <= n)
+                s =  '';
+               if isletter(str(i))
+                   while (i <= n)
+                      if isletter(str(i))
+                          s = strcat(s,str(i));
+                          i = i+1;
+                          continue;
+                      end
+                      if  ~isnan(str2double(str(i)))
+                          s = strcat(s,str(i));
+                          i = i+1;
+                          continue;
+                      end
+                      if str(i) == ' '
+                         s = strcat(s,str(i));
+                         i = i+1;
+                         continue;
+                      else
+                         break;
+                      end
+                      
+                   end
+                   arrVars{end+1} = s;
+               end
+               i = i+1;
+            end
+            arrVars =unique(sort(arrVars));
+            setGlobalArrVars(arrVars);
+            
+ 
+
+
+function eps_Callback(hObject, eventdata, handles)
+% hObject    handle to eps (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of eps as text
+%        str2double(get(hObject,'String')) returns contents of eps as a double
+
+
+% --- Executes during object creation, after setting all properties.
+function eps_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to eps (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in solve.
+function solve_Callback(hObject, eventdata, handles)
+contents = cellstr(get(handles.MethodChooser,'String'));
+choosen = contents{get(handles.MethodChooser,'Value')};
+if strcmp(choosen, 'Gauss-Jordan')
+    obj = GuassJordan();
+    obj.eqns = getGlobalArr;
+    obj.solve;
+    obj.b
+end
+if strcmp(choosen, 'Gauss')
+end
+if strcmp(choosen, 'LU')
+end
+if strcmp(choosen, 'Gauss-Seidel')
+end
